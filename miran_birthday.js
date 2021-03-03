679903a04bda009133f5041f8059fa44c1b8806f
devanshsrivastava06 <79897081+devanshsrivastava06@users.noreply.github.com>
2021-03-03 13:51:09 +0530

    Update miran_birthday.js

--- miran_birthday.js
+++ miran_birthday.js
@@ -4,7 +4,7 @@
           hour = minute * 60,
           day = hour * 24;
   
-    let birthday = "march 03, 2021 00:00:00",
+    let birthday = "march 09, 2021 00:00:00",
         countDown = new Date(birthday).getTime(),
         x = setInterval(function() {    
   
